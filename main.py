3479593868779102fcb16983a931bc68393c62f4
Fobos <126978771+MXcode970@users.noreply.github.com>
2024-04-10 16:43:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import discord
+from welcome import create_welcome_image
+import urllib.request
+
+client = discord.Client(intents=discord.Intents.all())
+
+async def send_welcome_message(member):
+    # Получение аватарки пользователя
+    url = member.avatar.url
+    
+    urllib.urlretrieve(url, f"avatar_{member.display_name}")
+
+    create_welcome_image("assets/black.jpg", f"avatar_{member.display_name}", f"{member.display_name} зашёл на сервер!",
+                         f"Участник #{len(member.guild.members)}", "res.png", "assets/NotoSans-Regular.ttf")
+    
+    # Отправка изображения в указанный канал
+    channel = discord.utils.get(member.guild.channels, name="welcome")
+    if channel:
+        with open("res.png", "rb") as image_file:
+            picture = discord.File(image_file)
+            await channel.send(file=picture)
+    else:
+        print("Error: Welcome channel not found.")
+
+# Обработчик события подключения нового участника
+@client.event
+async def on_member_join(member):
+    await send_welcome_message(member)
+
+# Обработчик события подключения нового участника
+@client.event
+async def on_member_join(member):
+    await send_welcome_message(member)
+
+# Запуск бота
+
+client.run('MTIyNzU5NzAwMTU4ODM0NjkwMQ.GADr_C.azjXuBu50f3kGOG1tKy8BLbUP5DcTtBNz5NHg8')
