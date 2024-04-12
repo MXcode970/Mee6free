78d816755d85c823354839a8b800040153c36232
Fobos <126978771+MXcode970@users.noreply.github.com>
2024-04-12 22:44:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from welcome import create_welcome_image
 import requests 
 import shutil
 import os
+import json
+
+
+with open('config.json', 'r', encoding='utf-8') as file:
+    config = json.load(file)
 
 bot = commands.Bot(command_prefix='/',intents=discord.Intents.all() )
 disabled = False
@@ -15,15 +20,22 @@ async def send_welcome_message(member):
     
     res = requests.get(url, stream = True)
 
+    file_path = f"avatar_{member.display_name}.png"
+
+
     if res.status_code == 200:
         with open(f"avatar_{member.display_name}.png",'wb') as f:
             shutil.copyfileobj(res.raw, f)
-        print('[MEE7] Avatar sucessfully Downloaded: ',f"avatar_{member.display_name}.png")
+        print(config["bot_prints"]["avatar_downloaded"],f"avatar_{member.display_name}.png")
     else:
-        print('[MEE7] [ERROR] Image Couldn\'t be retrieved')
+        print(config["bot_prints"]["avatar_download_error"])
 
-    create_welcome_image("assets/black.jpg", f"avatar_{member.display_name}", f"{member.display_name} зашёл на сервер!",
-                         f"Участник #{len(member.guild.members)}", "res.png", "assets/NotoSans-Regular.ttf")
+    create_welcome_image(
+        config['welcome_image_template']['background_image'], file_path,
+        f"{member.display_name} {config['bot_responses']['welcome_message']}",
+        f"{config['bot_responses']['member_count']}{len(member.guild.members)}", "res.png",
+        config['welcome_image_template']['font_path']
+    )
 
     channel = discord.utils.get(member.guild.channels, name="приветствие")
     if channel:
@@ -31,11 +43,11 @@ async def send_welcome_message(member):
             picture = discord.File(image_file)
             await channel.send(file=picture)
     else:
-        print("[MEE7] [ERROR] Welcome channel not found.")
+        print(config["bot_prints"]["welcome_channel_not_found"])
     try:
         os.remove(f"avatar_{member.display_name}.png")
     except Exception as e:
-        print(f"[MEE7] [ERROR] I cant delete avatar_{member.display_name}.png")
+        print(config["bot_prints"]["avatar_deleting_error"])
 
 @bot.event
 async def on_member_join(member):
@@ -47,32 +59,41 @@ async def on_member_join(member):
 
 
 
-@bot.tree.command(name="status", description="Поставить статус")
+@bot.tree.command(name="status", description=config['commands_description']['status'])
 async def status(ctx: discord.Integration, status: str):
     global disabled
     if disabled:
         return
 
-    if ctx.author.id == 728600636060467200:
-        await bot.change_presence(activity=discord.CustomActivity(name=status))
-        await ctx.response.send_message(f"Updated. **'{status}'**")
+    if ctx.user.id == config["ADMIN_ID"]:
+        if status == "standart":
+            await bot.change_presence(activity=discord.CustomActivity(name=config["default_status"]))
+            await ctx.response.send_message(config['bot_responses']['status_updated'])
+        else:
+            await bot.change_presence(activity=discord.CustomActivity(name=status))
+            await ctx.response.send_message(config['bot_responses']["status_custom_updated"])
     else:
-        print(f"[MEE7] {ctx.author.display_name} No rights")
+        print(config["bot_prints"]["no_rights_print"], ctx.user.display_name)
 
-@bot.command()
-async def disablemee7(ctx):
+@bot.tree.command(name="stop", description=config['commands_description']['stop'])
+async def stop(ctx: discord.Integration):
     global disabled
-    if ctx.author.id == 728600636060467200:
+    if ctx.user.id == config["ADMIN_ID"]:
         disabled = True
-        print("[MEE7] Disabled")
-        await bot.change_presence(activity=discord.CustomActivity(name="Disabled"), status=discord.Status.idle)
+        print(config["bot_prints"]["bot_disabled_print"])
+        await ctx.response.send_message(config["bot_responses"]["bot_disabled"])
+        await bot.change_presence(activity=discord.CustomActivity(name=config["disabeld_status"]), status=discord.Status.idle)
+
+
+
 
         
 @bot.event
 async def on_ready():
     print("[MEE7] Login!")
     await bot.tree.sync()
-    await bot.change_presence(activity=discord.CustomActivity(name="github.com/MXcode970/Mee6free"))
+    await bot.change_presence(activity=discord.CustomActivity(name=config["default_status"]))
+
 
 
 client.run('')
